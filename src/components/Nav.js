4740ee3461dc22df177f107fef1af791367d48ea
iamrainonline <64551360+iamrainonline@users.noreply.github.com>
2021-04-07 13:47:17 +0300

    changed the title

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ import { faMusic } from "@fortawesome/free-solid-svg-icons";
 const Nav = ({ setLibraryStatus, libraryStatus }) => {
   return (
     <nav>
-      <h1>Music</h1>
+      <h1>Music Player</h1>
       <button onClick={() => setLibraryStatus(!libraryStatus)}>
         Library <FontAwesomeIcon icon={faMusic} />{" "}
       </button>
