4c1208869b3eed2315a111da941f4235b83fe00c
unknown <cristiancandidatu@hotmail.com>
2024-07-20 18:33:55 +0300

    img

--- src/data.js
+++ src/data.js
@@ -1,88 +1,80 @@
 import { v4 as uuidv4 } from "uuid";
 
 function chillHop() {
-  return [
-    {
-      name: "Pueblo",
-      artist: "Philanthrope, tusken",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/10/efaa848553d09b4d9fc0752844456e41b34de276-1024x1024.jpg",
-      id: uuidv4(),
-      active: false,
-      color: ["#FBAC6C", "#0952B6"],
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=8788",
-    },
-    {
-      name: "Fox",
-      artist: "Sworn",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-      id: uuidv4(),
-      active: true,
-      color: ["#220A3E", "#C2487B"],
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=7814",
-    },
-    {
-      name: "Seascape",
-      artist: "Makzo",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg",
-      id: uuidv4(),
-      active: false,
-      color: ["#FBAC6C", "#0952B6"],
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=11773",
-    },
-    {
-      name: "Small Town Palm Trees",
-      artist: "Mama Aiuto",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2021/01/6b1bb8736ee3e972747bc11f312e31cf7f5823e4-1024x1024.jpg",
-      id: uuidv4(),
-      active: false,
-      color: ["#FBAC6C", "#0952B6"],
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=12125",
-    },
-    {
-      name: "Flowers",
-      artist: "The Filed Tapes, xander",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/11/8a0c857ddad531279d0757f5362380a6837b1b69-1024x1024.jpg",
-      id: uuidv4(),
-      active: false,
-      color: ["#FBAC6C", "#0952B6"],
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=11249",
-    },
-    {
-      name: "Going Back",
-      artist: "Swørn",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/10/737bb830d34592344eb4a2a1d2c006cdbfc811d9-1024x1024.jpg",
-      id: uuidv4(),
-      active: false,
-      color: ["#FBAC6C", "#0952B6"],
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=12125",
-    },
-    {
-      name: "Driftaway",
-      artist: "Tesk, No Spiri, Sitting Duck",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/08/63d2d2cdabbc5df023603b5f47b2fb97963cb537-1024x1024.jpg",
-      id: uuidv4(),
-      active: false,
-      color: ["#FBAC6C", "#0952B6"],
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9175",
-    },
-    {
-      name: "Bliss",
-      artist: "Misha, Jussi Halme",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/b6e48e6cfb2a90723b9cf1b108a6d305f9204eb4-1024x1024.jpg",
-      id: uuidv4(),
-      active: false,
-      color: ["#FBAC6C", "#0952B6"],
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9248",
-    },
-  ];
+   return [
+      {
+         name: "Pueblo",
+         artist: "Philanthrope, tusken",
+         cover: "https://images.unsplash.com/photo-1542397284385-6010376c5337?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+         id: uuidv4(),
+         active: false,
+         color: ["#FBAC6C", "#0952B6"],
+         audio: "https://mp3.chillhop.com/serve.php/?mp3=8788",
+      },
+      {
+         name: "Fox",
+         artist: "Sworn",
+         cover: "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+         id: uuidv4(),
+         active: true,
+         color: ["#220A3E", "#C2487B"],
+         audio: "https://mp3.chillhop.com/serve.php/?mp3=7814",
+      },
+      {
+         name: "Seascape",
+         artist: "Makzo",
+         cover: "https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg",
+         id: uuidv4(),
+         active: false,
+         color: ["#FBAC6C", "#0952B6"],
+         audio: "https://mp3.chillhop.com/serve.php/?mp3=11773",
+      },
+      {
+         name: "Small Town Palm Trees",
+         artist: "Mama Aiuto",
+         cover: "https://chillhop.com/wp-content/uploads/2021/01/6b1bb8736ee3e972747bc11f312e31cf7f5823e4-1024x1024.jpg",
+         id: uuidv4(),
+         active: false,
+         color: ["#FBAC6C", "#0952B6"],
+         audio: "https://mp3.chillhop.com/serve.php/?mp3=12125",
+      },
+      {
+         name: "Flowers",
+         artist: "The Filed Tapes, xander",
+         cover: "https://chillhop.com/wp-content/uploads/2020/11/8a0c857ddad531279d0757f5362380a6837b1b69-1024x1024.jpg",
+         id: uuidv4(),
+         active: false,
+         color: ["#FBAC6C", "#0952B6"],
+         audio: "https://mp3.chillhop.com/serve.php/?mp3=11249",
+      },
+      {
+         name: "Going Back",
+         artist: "Swørn",
+         cover: "https://chillhop.com/wp-content/uploads/2020/10/737bb830d34592344eb4a2a1d2c006cdbfc811d9-1024x1024.jpg",
+         id: uuidv4(),
+         active: false,
+         color: ["#FBAC6C", "#0952B6"],
+         audio: "https://mp3.chillhop.com/serve.php/?mp3=12125",
+      },
+      {
+         name: "Driftaway",
+         artist: "Tesk, No Spiri, Sitting Duck",
+         cover: "https://chillhop.com/wp-content/uploads/2020/08/63d2d2cdabbc5df023603b5f47b2fb97963cb537-1024x1024.jpg",
+         id: uuidv4(),
+         active: false,
+         color: ["#FBAC6C", "#0952B6"],
+         audio: "https://mp3.chillhop.com/serve.php/?mp3=9175",
+      },
+      {
+         name: "Bliss",
+         artist: "Misha, Jussi Halme",
+         cover: "https://chillhop.com/wp-content/uploads/2020/07/b6e48e6cfb2a90723b9cf1b108a6d305f9204eb4-1024x1024.jpg",
+         id: uuidv4(),
+         active: false,
+         color: ["#FBAC6C", "#0952B6"],
+         audio: "https://mp3.chillhop.com/serve.php/?mp3=9248",
+      },
+   ];
 }
 
 export default chillHop;
