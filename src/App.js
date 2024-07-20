16708bc7d6264785438ab392b38e7f3582718189
unknown <cristiancandidatu@hotmail.com>
2024-07-20 18:37:51 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -10,58 +10,61 @@ import Nav from "./components/Nav";
 import data from "./data";
 
 const App = () => {
-  // State
-  const [songInfo, setSongInfo] = useState({
-    currentTime: 0,
-    duration: 0,
-  });
-  const [libraryStatus, setLibraryStatus] = useState(false);
-  const timeUpdateHandler = (e) => {
-    const current = e.target.currentTime;
-    const duration = e.target.duration;
-    setSongInfo({ ...songInfo, currentTime: current, duration });
-  };
-  const audioRef = useRef(null);
-  const [songs, setSongs] = useState(data());
-  const [currentSong, setCurrentSong] = useState(songs[1]);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const songEndHandler = async () => {
-    let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
-    await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
-    if (isPlaying) audioRef.current.play();
-  };
-  return (
-    <div className={`App ${libraryStatus ? "library-active" : ""}`}>
-      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
-      <Song currentSong={currentSong} />
-      <Player
-        setSongInfo={setSongInfo}
-        songInfo={songInfo}
-        audioRef={audioRef}
-        isPlaying={isPlaying}
-        setIsPlaying={setIsPlaying}
-        currentSong={currentSong}
-        songs={songs}
-        setCurrentSong={setCurrentSong}
-        setSongs={setSongs}
-      />
-      <Library
-        libraryStatus={libraryStatus}
-        audioRef={audioRef}
-        songs={songs}
-        setCurrentSong={setCurrentSong}
-        isPlaying={isPlaying}
-        setSongs={setSongs}
-      />
-      <audio
-        onTimeUpdate={timeUpdateHandler}
-        onLoadedMetadata={timeUpdateHandler}
-        ref={audioRef}
-        src={currentSong.audio}
-        onEnded={songEndHandler}
-      ></audio>
-    </div>
-  );
+   // State
+   const [songInfo, setSongInfo] = useState({
+      currentTime: 0,
+      duration: 0,
+   });
+   const [libraryStatus, setLibraryStatus] = useState(true);
+   const timeUpdateHandler = (e) => {
+      const current = e.target.currentTime;
+      const duration = e.target.duration;
+      setSongInfo({ ...songInfo, currentTime: current, duration });
+   };
+   const audioRef = useRef(null);
+   const [songs, setSongs] = useState(data());
+   const [currentSong, setCurrentSong] = useState(songs[1]);
+   const [isPlaying, setIsPlaying] = useState(false);
+   const songEndHandler = async () => {
+      let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+      await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+      if (isPlaying) audioRef.current.play();
+   };
+   return (
+      <div className={`App ${libraryStatus ? "library-active" : ""}`}>
+         <Nav
+            libraryStatus={libraryStatus}
+            setLibraryStatus={setLibraryStatus}
+         />
+         <Song currentSong={currentSong} />
+         <Player
+            setSongInfo={setSongInfo}
+            songInfo={songInfo}
+            audioRef={audioRef}
+            isPlaying={isPlaying}
+            setIsPlaying={setIsPlaying}
+            currentSong={currentSong}
+            songs={songs}
+            setCurrentSong={setCurrentSong}
+            setSongs={setSongs}
+         />
+         <Library
+            libraryStatus={libraryStatus}
+            audioRef={audioRef}
+            songs={songs}
+            setCurrentSong={setCurrentSong}
+            isPlaying={isPlaying}
+            setSongs={setSongs}
+         />
+         <audio
+            onTimeUpdate={timeUpdateHandler}
+            onLoadedMetadata={timeUpdateHandler}
+            ref={audioRef}
+            src={currentSong.audio}
+            onEnded={songEndHandler}
+         ></audio>
+      </div>
+   );
 };
 
 export default App;
